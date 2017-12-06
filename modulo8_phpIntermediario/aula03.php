a99ff33d6b20513c8d0a67354d6a5373a7533fc8
Carlos Andre C <carlosandrecouto@live.com>
2017-12-06 18:12:41 -0200

    Aula 02 - Foreach

---
+++ modulo8_phpIntermediario/aula03.php
@@ -0,0 +1,41 @@
+<?php
+	//Aula 02 - Foreach
+	header("Content-type: text/html; charset=utf-8");
+
+	$nomes = array("Carlos", "Andre", "Vinicios", "William");
+
+	foreach ($nomes as $aluno) {
+		echo "Aluno: ".$aluno."<br/>";
+	};
+
+	$usersList = array(
+		array(
+			'nome' => "Carlos",
+			"idade" => "22",
+			"profissao" => "Front-End Plenin"
+		),
+		array(
+			'nome' => "William",
+			"idade" => "26",
+			"profissao" => "PHP Senior Foda"
+		),
+		array(
+			'nome' => "Vinicios",
+			"idade" => "27",
+			"profissao" => "Av. Tapajós Senior"
+		)
+	);
+
+	//Exemplo 01
+	foreach ($usersList as $users) {
+		echo "
+			<p>Nome: ".$users["nome"]."<p>
+			<p>Idade: ".$users["idade"]."<p>
+			<p>Profissao: ".$users["profissao"]."<p><hr>"
+		;
+	};
+
+	foreach ($usersList as $dados => $value) {
+		echo "<p>".$dados." =".$value["nome"]." = ".$value["idade"]." ".$value["profissao"]."<p>";
+	}
+?>
