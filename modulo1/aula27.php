bee7c3cdc2fa4789ee8de96bd4f4c10e9e82ec7b
Carlos Andre C <carlosandrecouto@live.com>
2017-12-06 12:22:33 -0200

    Aula 27 - Tipos de variáveis

---
+++ modulo1/aula27.php
@@ -0,0 +1,9 @@
+<?php
+	//Aula 27 - Tipos de variáveis
+
+	$name = "Carlos"; //String ""
+	$idade = 22; //Inteiro, int, integer
+	$nota = 7.5; //float
+	$ligado = true; //Boolean true/false
+	$caracteristicas = array("Alegre", "Feliz"); //array
+ ?>
